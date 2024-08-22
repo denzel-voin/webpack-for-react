8d9800590d0fb7ac9cdc40993b05b88c79dc2f4f
Denis Voinovich <denzelvojn@gmail.com>
2024-08-22 22:00:48 +0300

    add configuration

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+const numbers:number[] = [2, 3, 5];
+
+const doubledNumbers = numbers.map(number => number * 2);
+
+console.log(doubledNumbers);
--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,37 @@
-const path = require('path');
+const path = require('path'); //для того чтобы превратить относительный путь в абсолютный, мы будем использовать пакет path
+const HTMLWebpackPlugins = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: './path/to/my/entry/file.js',
+  entry: path.resolve(__dirname, './src/index.ts'), //точка входа в наше приложение содержит абсолютный путь к index.ts
   output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: 'bundle.js',
+    path: path.resolve(__dirname, './dist'), //путь, куда будет собираться наш проект
+    filename: 'main.js', // имя нашего бандла
   },
+  plugins: [
+    new HTMLWebpackPlugins({
+      template: path.resolve(__dirname, 'public/index.html'),
+      inject: "body"
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  //Нужно помочь вебпаку научиться работать с jsx- и tsx-файлами, для этого используют ts-loader
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
+        test: /\.[tj]sx?$/, //содержит регулярное выражение, которое содержит информацию, какие файлы должны обрабатываться этим loader'ом
+        use: [
+          {
+            loader: 'ts-loader',
+          },
+        ], // для того чтобы ts-loader корректно отработал, нам нужен tsconfig, его можно создать вручную, а можно создать автоматически
+        /** чтобы проинициализировать его автоматически, можно установить пакет typesctipt глобально или использовать npx, выполнив команду npx tsc --init
+         После создания конфига нужно включить "allowJs": true, чтобы работать не только c typescript, также меняем "jsx": "react", чтобы мы могли работать с react-компонентами, и включаем карту ресурсов "sourceMap": true, пока на этом все вернёмся в этот конфиг позже*/
         exclude: /node_modules/,
-        use: [ ],
       },
     ],
   },
   resolve: {
-    extensions: ['*', '.js', '.jsx'],
-  },
-
-  devServer: {
-    static: path.resolve(__dirname, './dist'),
-    compress: true,
-    port: 8080,
-    open: true,
-    hot: true,
+    extensions: ['.js', '.jsx', '.tsx', '.ts', '.json'], //указываем файлы, с которыми будет работать webpack
   },
 };
